e396ee36306b53e153f45008aa92b821616a57ec
Tal Zion <talezion@gmail.com>
2024-12-18 18:43:32 +0200

    Fragments

--- app/src/main/java/com/idz/colman24class2/StudentsRecyclerViewActivity.kt
+++ app/src/main/java/com/idz/colman24class2/StudentsRecyclerViewActivity.kt
@@ -2,17 +2,13 @@ package com.idz.colman24class2
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.idz.colman24class2.adapter.StudentsRecyclerAdapter
 import com.idz.colman24class2.model.Model
 import com.idz.colman24class2.model.Student
 
@@ -40,7 +36,7 @@ class StudentsRecyclerViewActivity : AppCompatActivity() {
         // TODO: 3. Create ViewHolder ✅
 
         students = Model.shared.students
-        val recyclerView: RecyclerView = findViewById(R.id.students_recycler_view)
+        val recyclerView: RecyclerView = findViewById(R.id.students_list_activity_recycler_view)
         recyclerView.setHasFixedSize(true)
 
         val layoutManager = LinearLayoutManager(this)
@@ -60,62 +56,5 @@ class StudentsRecyclerViewActivity : AppCompatActivity() {
 
         recyclerView.adapter = adapter
 
-    }
-
-    class StudentViewHolder(
-        itemView: View,
-        listener: OnItemClickListener?
-    ): RecyclerView.ViewHolder(itemView) {
-
-        private var nameTextView: TextView? = null
-        private var idTextView: TextView? = null
-        private var checkBox: CheckBox? = null
-        private var student: Student? = null
-
-        init {
-            nameTextView = itemView.findViewById(R.id.student_row_name_text_view)
-            idTextView = itemView.findViewById(R.id.student_row_id_text_view)
-            checkBox = itemView.findViewById(R.id.student_row_check_box)
-
-            checkBox?.apply {
-                setOnClickListener { view ->
-                    (tag as? Int)?.let { tag ->
-                        student?.isChecked = (view as? CheckBox)?.isChecked ?: false
-                    }
-                }
-            }
-
-            itemView.setOnClickListener {
-                listener?.onItemClick(adapterPosition)
-                listener?.onItemClick(student)
-            }
-        }
-
-        fun bind(student: Student?, position: Int) {
-            this.student = student
-            nameTextView?.text = student?.name
-            idTextView?.text = student?.id
-            checkBox?.apply {
-                isChecked = student?.isChecked ?: false
-                tag = position
-            }
-        }
-    }
-
-    class StudentsRecyclerAdapter(private val students: List<Student>?): RecyclerView.Adapter<StudentViewHolder>() {
-
-        var listener: OnItemClickListener? = null
-
-        override fun getItemCount(): Int = students?.size ?: 0
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
-            val inflation = LayoutInflater.from(parent.context)
-            val view = inflation.inflate(R.layout.student_list_row, parent, false)
-            return StudentViewHolder(view, listener)
-        }
-
-        override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-            holder.bind(students?.get(position), position)
-        }
     }
 }
